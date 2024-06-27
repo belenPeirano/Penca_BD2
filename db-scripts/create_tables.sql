c8bb12e92d5128431fc23bc01b0697a281370760
belenPeirano <maria.peirano@correo.ucu.edu.uy>
2024-06-26 21:30:51 -0300

    scripts bd

---
+++ db-scripts/create_tables.sql
@@ -0,0 +1,76 @@
+    drop database pencabdii;
+    create database pencabdii;
+
+    use pencabdii;
+
+    CREATE TABLE usuario (
+    ci varchar(10) NOT NULL,
+    nombre varchar(50) NOT NULL,
+    apellido varchar(50) NOT NULL,
+    email varchar(30) NOT NULL unique,
+    psw varchar(100) NOT NULL,
+    rol varchar(20) NOT NULL default "ESTUDIANTE",
+    PRIMARY KEY (ci)
+    ) DEFAULT CHARSET=utf8;
+
+    CREATE TABLE carrera (
+    id_carrera int auto_increment NOT NULL,
+    nombre varchar(50) NOT NULL,
+    PRIMARY KEY(id_carrera)
+    ) DEFAULT CHARSET=utf8;
+
+    CREATE TABLE equipo (
+    id_equipo int auto_increment NOT NULL,
+    nombre varchar(50) NOT NULL,
+    PRIMARY KEY(id_equipo)
+    ) DEFAULT CHARSET=utf8;
+
+    CREATE TABLE estudiante (
+    ci varchar(10) NOT NULL,
+    id_carrera int NOT NULL,
+    puntaje_total int NOT NULL default 0,
+    predic_campeon int NOT NULL,
+    predic_subcampeon int NOT NULL,
+    PRIMARY KEY(ci),
+    CONSTRAINT FOREIGN KEY(ci) REFERENCES usuario(ci),
+    CONSTRAINT FOREIGN KEY(id_carrera) REFERENCES carrera(id_carrera),
+    CONSTRAINT FOREIGN KEY(predic_campeon) REFERENCES equipo(id_equipo),
+    CONSTRAINT FOREIGN KEY(predic_subcampeon) REFERENCES equipo(id_equipo)
+    ) DEFAULT CHARSET=utf8;
+
+    CREATE TABLE fase (
+    id_fase int auto_increment NOT NULL,
+    nombre varchar(20) NOT NULL,
+    PRIMARY KEY(id_fase)
+    ) DEFAULT CHARSET=utf8;
+
+    CREATE TABLE partido (
+    id_partido int auto_increment NOT NULL,
+    fecha datetime,
+    lugar varchar(100),
+    fase int NOT NULL,
+    equipo_local int,
+    equipo_visitante int,
+    result_local int,
+    result_visitante int,
+    PRIMARY KEY(id_partido),
+    CONSTRAINT FOREIGN KEY(equipo_local) REFERENCES equipo(id_equipo),
+    CONSTRAINT FOREIGN KEY(equipo_visitante) REFERENCES equipo(id_equipo),
+    CONSTRAINT FOREIGN KEY(fase) REFERENCES fase(id_fase),
+    CONSTRAINT CHK_PARTIDO check (equipo_local<>equipo_visitante)
+    ) DEFAULT CHARSET=utf8;
+
+
+    CREATE TABLE prediccion (
+    id_prediccion int auto_increment NOT NULL,
+    id_partido int NOT NULL,
+    ci_estudiante varchar(10) NOT NULL,
+    equipo_ganador int NOT NULL,
+    result_local int NOT NULL,
+    result_visitante int NOT NULL,
+    puntaje int NOT NULL,
+    PRIMARY KEY(id_prediccion),
+    CONSTRAINT FOREIGN KEY(equipo_ganador) REFERENCES equipo(id_equipo),
+    CONSTRAINT FOREIGN KEY(id_partido) REFERENCES partido(id_partido),
+    CONSTRAINT FOREIGN KEY(ci_estudiante) REFERENCES estudiante(ci)
+    ) DEFAULT CHARSET=utf8;
